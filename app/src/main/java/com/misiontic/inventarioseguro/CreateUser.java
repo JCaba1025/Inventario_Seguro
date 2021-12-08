33bdf90554f90c5ab07c66c2ab6fbbfdea0a0e0f
JCaba1025 <jorge.o.cardenas.c@gmail.com>
2021-12-08 00:09:27 -0500

    Se crea Activity Main y Activity CreateUser, solo FrontEnd

---
+++ app/src/main/java/com/misiontic/inventarioseguro/CreateUser.java
@@ -0,0 +1,19 @@
+package com.misiontic.inventarioseguro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
+
+    public void SingUp(View view){
+
+    }
+}
--- app/src/main/java/com/misiontic/inventarioseguro/MainActivity.java
+++ app/src/main/java/com/misiontic/inventarioseguro/MainActivity.java
@@ -2,7 +2,11 @@ package com.misiontic.inventarioseguro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void CrearUsuario(View view){
+
+        Intent a = new Intent(this, CreateUser.class);
+        startActivity(a);
+
+    }
+
+    public void login(View view){
+
+        /*UserApp ConectionDB = new UserApp(this);
+        String sentence ="SELECT*FROM userApp";
+
+        //Cursor results = ConectionDB.getData(sentence, null);
+
+        //if (results.getCount()==0){
+
+            Toast.makeText(this, "No hay usuarios registrados", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Por favor registre un usuario", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            Intent b = new Intent(this, Login.class);
+            startActivity(b);
+
+        }*/
+
+    }
+
 }
